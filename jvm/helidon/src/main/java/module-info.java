959ce7c70eac3314c038a97f51a0488f7627256c
Mark Pippins <mpippins@gmail.com>
2026-05-20 23:53:47 -0400

    pom fixes

--- jvm/helidon/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.aibizarchitect.nexus.v1.servicebroker.api {
-    requires transitive io.clientcore.core;
-
-    exports com.aibizarchitect.nexus.v1.servicebroker.api;
-
-    opens com.aibizarchitect.nexus.v1.servicebroker.api to io.clientcore.core;
-}
